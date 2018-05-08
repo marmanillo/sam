f16ac97ed7614806747ea512359dd3019d1c3062
marmanillo <erick28_06@hotmail.com>
2018-05-08 13:10:47 +0200

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
   def new
+    @user = @user = User.new
+    #debugger
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      flash[:alert] = "Complete the field!!"
+      render 'new'
+    end
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
